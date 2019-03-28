c2c9c88fa9220d797084d020611b11780e2eb5ff
MJ <mj662560@gmail.com>
2019-03-27 20:36:30 -0400

    Created HTML templat
    Created HTML template for one of the url's and attached the template to the view

--- firstwebsite/music/urls.py
+++ firstwebsite/music/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from music import views
 
 urlpatterns = [
-    url(r'^/',views.index, name='index'),
+    url(r'^$',views.index, name='index'),
     #/music/(album id)/
     url(r'^(?P<album_id>[0-9]+)/$',views.detail),
 
--- firstwebsite/music/views.py
+++ firstwebsite/music/views.py
@@ -5,10 +5,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Album
 from django.template import loader
-
 def index(request):
-    return HttpResponse("<h2>Filler Text</h2>")
+    all_albums = Album.objects.all()
+    print (all_albums)
 
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums
+    }
+    #return HttpResponse("<h2>Details for Album id </h2>")
+    #return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
+    #html =''
+    #for album in all_albums:
+    #    url = '/music/' + str(album.id) + '/'
+    #    html += '<a href="'+ url + '">'+ album.album_title+'</a><br>'
+    #<li><a href="/music/1/"> text</a></li>
+
+    return HttpResponse(html)
 def detail(request, album_id):
     print str(album_id)
     return HttpResponse("<h2>" +str(album_id) + "</h2>")
