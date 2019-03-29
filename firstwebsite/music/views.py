ea45f09406d4a8f67804f5c935a2b3c91338efb2
MJ <mj662560@gmail.com>
2019-03-28 20:22:24 -0400

    Added 404 error
    Added 404 error for invalid album id's

--- firstwebsite/music/views.py
+++ firstwebsite/music/views.py
@@ -1,29 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponse
 from .models import Album
-from django.template import loader
+from django.shortcuts import render
+
 def index(request):
     all_albums = Album.objects.all()
-    print (all_albums)
-
-    template = loader.get_template('music/index.html')
     context = {
         'all_albums': all_albums
     }
-    #return HttpResponse("<h2>Details for Album id </h2>")
-    #return HttpResponse(template.render(context,request))
-    return HttpResponse(template.render(context, request))
-    #html =''
-    #for album in all_albums:
-    #    url = '/music/' + str(album.id) + '/'
-    #    html += '<a href="'+ url + '">'+ album.album_title+'</a><br>'
-    #<li><a href="/music/1/"> text</a></li>
+    return render(request, 'music/index.html', context)
 
-    return HttpResponse(html)
 def detail(request, album_id):
-    print str(album_id)
-    return HttpResponse("<h2>" +str(album_id) + "</h2>")
-# Create your views here.
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+
+    return render(request, 'music/detail.html', {'album': album})
