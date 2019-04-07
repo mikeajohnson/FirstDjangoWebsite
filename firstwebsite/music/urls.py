df696f094bb453f861b944bae4605e6e9f041996
MJ <mj662560@gmail.com>
2019-04-06 22:25:07 -0400

    Improved HTML template and use 404 shortcut
    Added to the details template, added a url namespace and removed hard coded url's in template, and used 404 shortcut instead of try-except

--- firstwebsite/music/urls.py
+++ firstwebsite/music/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from music import views
 
+app_name = 'music'
+
 urlpatterns = [
     url(r'^$',views.index, name='index'),
     #/music/(album id)/
-    url(r'^(?P<album_id>[0-9]+)/$',views.detail),
+    url(r'^(?P<album_id>[0-9]+)/$',views.detail, name='detail'),
 
 ]
--- firstwebsite/music/views.py
+++ firstwebsite/music/views.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import Http404
 from django.http import HttpResponse
 from .models import Album
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     all_albums = Album.objects.all()
@@ -13,9 +12,5 @@ def index(request):
     return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
-
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
