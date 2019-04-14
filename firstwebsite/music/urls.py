922288a40314efec6f6499aea140085dde888aa9
MJ <mj662560@gmail.com>
2019-04-13 22:41:08 -0400

    Added radio buttons
    Added radio buttons to the index template

--- firstwebsite/music/urls.py
+++ firstwebsite/music/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     url(r'^$',views.index, name='index'),
     #/music/(album id)/
     url(r'^(?P<album_id>[0-9]+)/$',views.detail, name='detail'),
+    #/music/(album id)/favorite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$',views.favorite, name='favorite'),
 
 ]
--- firstwebsite/music/views.py
+++ firstwebsite/music/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
-from .models import Album
+from .models import Album, Song
 from django.shortcuts import render, get_object_or_404
 
 def index(request):
@@ -14,3 +14,17 @@ def index(request):
 def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html',{
+        'album': album,
+        'error_message': "ERROR"
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
